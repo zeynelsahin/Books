b14b9310b716a0c60f909bdecedfad7b886efde2
zeynelsahin <zynl.shn@hotmail.com>
2023-02-09 22:10:51 +0300

    Initial commit

---
+++ Books.API/Entities/Author.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Books.API.Entities;
+[Table("Authors")]
+public class Author
+{
+    [Key]
+    public Guid Type { get; set; }
+
+    [Required]
+    [MaxLength(150)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(150)]
+    public string LastName { get; set; }
+
+    public Author(Guid type, string name, string lastName)
+    {
+        Type = type;
+        Name = name;
+        LastName = lastName;
+    }
+}
---
+++ Books.API/Program.cs
@@ -0,0 +1,29 @@
+using Books.API.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BooksContext>(optionsAction => optionsAction.UseSqlite(builder.Configuration["ConnectionStrings:BooksDBConnectionString"]));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
